de90108a0412048c93aaa8d55b46c6ad8298faa2
czj712 <148737432+czj712@users.noreply.github.com>
2024-06-14 15:34:20 +0800

    Update merge_comments.py

--- czjDataSetsPrj/merge_comments.py
+++ czjDataSetsPrj/merge_comments.py
@@ -16,6 +16,7 @@ for user_data in data:
             "product_id": comment["parent_asin"],
             "review": comment["text"]
         })
+        print(user_info)
     # 将当前用户的字典添加到用户评论列表中
     user_comments.append(user_info)
 
