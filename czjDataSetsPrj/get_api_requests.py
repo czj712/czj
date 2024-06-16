c65c7cf9ee0627cbaf49400c96cc3c7001d1b6db
czj712 <148737432+czj712@users.noreply.github.com>
2024-06-16 13:38:58 +0800

    Update get_api_requests.py

--- czjDataSetsPrj/get_api_requests.py
+++ czjDataSetsPrj/get_api_requests.py
@@ -5,10 +5,11 @@ import os
 
 # 设置文件路径和对应的类别
 file_paths = {
-    '/users/u202220081001066/datas/All_Beauty.jsonl': 'All_Beauty',
-    '/users/u202220081001066/datas/Handmade_Products.jsonl': 'Handmade_Products',
-    '/users/u202220081001066/datas/Musical_Instruments.jsonl': 'Musical_Instruments',
-    '/users/u202220081001066/datas/Software.jsonl': 'Software'
+    '/Users/zijianchen/Desktop/datas/All_Beauty.jsonl': 'All_Beauty',
+    '/Users/zijianchen/Desktop/datas/Video_Games.jsonl': 'Video_Games',
+    '/Users/zijianchen/Desktop/datas/Musical_Instruments.jsonl': 'Musical_Instruments',
+    '/Users/zijianchen/Desktop/datas/Software.jsonl': 'Software' , 
+    '/Users/zijianchen/Desktop/datas/Industrial_and_Scientific.jsonl': 'Industrial_and_Scientific' 
 }
 
 # 读取JSONL文件并提取数据，同时添加category字段
@@ -21,7 +22,7 @@ for file_path, category in file_paths.items():
             all_data.append(data)
 
 # 随机选取500条数据
-sampled_data = random.sample(all_data, 1000)
+sampled_data = random.sample(all_data, 500)
 
 api_key = "9d207bf0-10f5-4d8f-a479-22ff5aeffad1"
 
@@ -40,9 +41,9 @@ for index, item in enumerate(sampled_data, 1):
         "userid": userid,
         "category": category,
         "display_fields": "user_id,text",
-        "max_chars_len": 500,
+        "max_chars_len": 800,
         "order_by_helpfulvote": "true",
-        "max_len": 5
+        "max_len": 1
     }
     # 打印请求体
     print(f"Request payload: {json.dumps(payload, indent=2)}")
@@ -57,7 +58,7 @@ for index, item in enumerate(sampled_data, 1):
     print(f"Response: {json.dumps(result, indent=2)}")
 
 # 保存结果到JSON文件
-with open('/users/u202220081001066/datas/5comment_result.json', 'w') as f:
+with open('/Users/zijianchen/Desktop/datas/1comment_result.json', 'w') as f:
     json.dump(results, f)
 
-print("All data processed and saved to 5comment_result.json.")
+print("All data processed and saved to 1comment_result.json.")
