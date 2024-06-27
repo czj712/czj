644a9de593c6ad3f169b1d24d7e921b3c8a5ce69
czj712 <148737432+czj712@users.noreply.github.com>
2024-06-27 17:15:30 +0800

    Update and rename qlora_train_phi.py to lora_train_phi.py

--- lora/lora_train_phi.py
+++ lora/lora_train_phi.py
@@ -6,22 +6,26 @@ import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM, DataCollatorForLanguageModeling, BitsAndBytesConfig
 import os
 import json
+import wandb
 
-# 使用QLoRA量化加载模型
-bnb_config = BitsAndBytesConfig(
-    load_in_8bit=True,
-    llm_int8_threshold=6.0,
-    llm_int8_enable_fp32_cpu_offload = False,
-    llm_int8_skip_modules = None
-    )
+wandb.init(project="phi3_lora_finetuning")
+def check_model_layers(model):
+    layer_shapes = {}
+    for name, module in model.named_modules():
+        if isinstance(module, nn.Linear):
+            layer_shapes[name] = module.weight.shape
+    return layer_shapes
+
+layer_shapes = check_model_layers(model)
+for name, shape in layer_shapes.items():
+    print(f"Layer: {name}, Shape: {shape}")
+
 # 加载预训练的 phi-3 模型
-model_id = "microsoft/Phi-3-mini-4k-instruct"
+model_id = "/home/u202220081001066/phi3"
 model = AutoModelForCausalLM.from_pretrained(model_id,
-    quantization_config=bnb_config,
     use_cache=False,
     trust_remote_code=True,
-    attn_implementation="flash_attention_2",  # loading the model with flash-attenstion support
-    torch_dtype=torch.bfloat16,
+    torch_dtype=torch.float16,
     device_map="auto")
 try:
     with open(model_id, 'r') as f:
@@ -38,7 +42,7 @@ tokenizer.pad_token_id = tokenizer.convert_tokens_to_ids(tokenizer.pad_token)
 tokenizer.padding_side = 'right'
 
 # 加载数据集
-data_file_path = "//home/ubuntu/czjDataSetsPrj/single_review_rp_gpt_outputs.json"
+data_file_path = "/users/u202220081001066/datas/single_review_rp_gpt_outputs.json"
 data = load_dataset("json",data_files=data_file_path, split='train')
 data = data.shuffle(seed=1234)  # 打乱数据集
 
@@ -50,21 +54,18 @@ test_data = data["test"]
 
 # PeFT 配置
 lora_config = LoraConfig(
-    r=16,
-    lora_alpha=32,
+    r=32,
+    lora_alpha=64,
     target_modules=["q_proj", "k_proj", "v_proj","o_proj"],
     lora_dropout=0.05,
     bias="none",
     task_type="CAUSAL_LM"
 )
 
-# 准备模型进行 k 位训练
-model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, lora_config)
+model.print_trainable_parameters()
 
-# 添加 Lora 适配器
-model.add_adapter(lora_config, adapter_name="adapter")
-
-output_dir = "/home/ubuntu/outputs"
+output_dir = "/users/u202220081001066/outputs"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
@@ -79,12 +80,12 @@ trainer = SFTTrainer(
     max_seq_length=2048,
     tokenizer=tokenizer,
     args=transformers.TrainingArguments(
-        num_train_epochs= 10,
+        num_train_epochs= 5,
         per_device_train_batch_size=4,
-        gradient_accumulation_steps=12,
+        gradient_accumulation_steps=4,
         warmup_steps=1,
-        max_steps=100,
-        learning_rate=2e-4,
+        max_steps=-1,
+        learning_rate=4e-4,
         bf16=True,
         logging_steps=1,
         output_dir=output_dir,
@@ -100,19 +101,7 @@ trainer.train()
 adapter_output_dir = os.path.join(output_dir, "phi3_adapter")
 if not os.path.exists(adapter_output_dir):
     os.makedirs(adapter_output_dir)
+
 model.save_pretrained(adapter_output_dir)
-
-# 加载 LoRA 适配器并合并到模型中
-adapter_path = adapter_output_dir  # 使用保存的适配器路径
-model = PeftModel.from_pretrained(model, adapter_path)
-
-# 将合并后的模型保存到本地
-merged_output_dir = os.path.join(output_dir, "phi3_merged")
-if not os.path.exists(merged_output_dir):
-    os.makedirs(merged_output_dir)
-model.save_pretrained(merged_output_dir)
-tokenizer.save_pretrained(merged_output_dir)
-
-model.push_to_hub("dickdiss/new_phi-3_qlora")
-tokenizer.push_to_hub("dickdiss/new_phi-3_qlora")
-
+tokenizer.save_pretrained(adapter_output_dir)
+wandb.finish()
